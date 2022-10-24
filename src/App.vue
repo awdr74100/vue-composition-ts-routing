fa860c4715deb25b039914e661a30fc1c1a37abf
Yi-Ru Lan <a78945612385238@gmail.com>
2022-10-25 05:11:48 +0800

    Update ...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
-<template></template>
+<template>
+  <nav>
+    <RouterLink to="/">首頁</RouterLink>
+    <RouterLink to="/about?a=a&b=b">關於</RouterLink>
+  </nav>
+  <nav>
+    <RouterLink :to="{ path: '/' }">首頁</RouterLink>
+    <RouterLink :to="{ path: '/about', query: { a: 'aa', b: 'bb' } }">關於</RouterLink>
+  </nav>
+  <nav>
+    <button @click.prevent="pushWithQuery('/')">首頁</button>
+    <button @click.prevent="pushWithQuery('/about', { a: 'aaa', b: 'bbb' })">關於</button>
+  </nav>
+  <nav>
+    <a href="#" @click.prevent="router.push('/')">首頁</a>
+    <a href="#" @click.prevent="router.push('/about?a=aaaa&b=bbbb')">關於</a>
+  </nav>
+  <RouterView />
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { watch } from 'vue';
+import { RouterLink, RouterView, useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+
+const pushWithQuery = (path: string, query?: { [prop: string]: string }) => {
+  router.push({ path, query });
+};
+
+watch(
+  () => route.query.a,
+  (a) => {
+    console.log(a);
+  },
+);
+</script>
 
 <style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>HomeView Component</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
